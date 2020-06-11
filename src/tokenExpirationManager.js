df710d7983203d5d308c44c0e8f902f1b4e0a04a
Gwenael Pluchon <accounts@gwenp.fr>
2020-06-11 13:15:07 +0200

    fix(token): token in db errors

--- src/tokenExpirationManager.js
+++ src/tokenExpirationManager.js
@@ -3,7 +3,7 @@ module.exports = {
     const adapter = require('./options').get().adapter;
     return {
       'passport-mongo': require('./adapters/passport-mongo/tokenExpirationManager'),
-      'passport-postgre': require('./adapters/passport-postgres/tokenExpirationManager'),
+      'passport-postgre': require('./adapters/passport-postgre/tokenExpirationManager'),
     }[adapter];
   },
   newTokenForUser(username, token) {
@@ -12,8 +12,10 @@ module.exports = {
   isTokenValid(tokenToCheck) {
     return this.getManager().isTokenValid(tokenToCheck);
   },
-  listTokens() {
-    return this.getManager().listTokens();
+  async listTokens() {
+    const tokens = await this.getManager().listTokens();
+    console.log('listTokens', tokens);
+    return tokens;
   },
   revokeToken(token) {
     return this.getManager().revokeToken(token);
